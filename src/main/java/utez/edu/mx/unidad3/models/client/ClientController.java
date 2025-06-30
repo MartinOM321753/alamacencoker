a1e73c0122a2e52bcf9b8c046772c3fe456021b1
MartinOM321753 <20223tn139@utez.edu.mx>
2025-06-30 16:45:56 -0600

    [ADD] métodos para encontrar y guardar clientes en el servicio y controlador, incluyendo documentación con Swagger

--- src/main/java/utez/edu/mx/unidad3/models/client/ClientController.java
+++ src/main/java/utez/edu/mx/unidad3/models/client/ClientController.java
@@ -1,23 +1,97 @@
 package utez.edu.mx.unidad3.models.client;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import utez.edu.mx.unidad3.utlis.APIResponse;
 
 @RestController
 @RequestMapping("/api/client")
+@Tag(name = "Controlador de clientes", description = "Operaciones relacionadas con clientes")
 public class ClientController {
+
     @Autowired
     private ClientService clientService;
 
     @GetMapping("")
-    public ResponseEntity<APIResponse> findAll(){
+    @Operation(summary = "Traer clientes", description = "Retorna todos los clientes registrados en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Traer todos los clinetes",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = APIResponse.class)),
+                            @Content(mediaType = "application/xml", schema = @Schema(implementation = APIResponse.class)),
+                            @Content(mediaType = "application/html", schema = @Schema(implementation = APIResponse.class))
+                    }),
+    })
+    public ResponseEntity<APIResponse> findAll() {
 
         APIResponse response = clientService.findAll();
-        return new ResponseEntity<>(response ,response.getStatus());
+        return new ResponseEntity<>(response, response.getStatus());
     }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Traer un cliente", description = "Retorna al cliente registrado en el sistema")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Traer un clinete",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = APIResponse.class)),
+
+                    }),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se encontro al cliente",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = APIResponse.class)),
+
+                    }),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "No se pudo consultar al ciente",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = APIResponse.class)),
+
+                    }),
+    })
+    public ResponseEntity<APIResponse> findById(@PathVariable("id") Long id) {
+        APIResponse response = clientService.findById(id);
+        return new ResponseEntity<>(response, response.getStatus());
+
+
+    }
+
+    @PostMapping("/save")
+    @Operation(summary = "Guardar al cliente", description = "Registar la informacion de un cliente")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Registro exitoso del cliente",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = APIResponse.class)),
+
+                    }),
+
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "No se pudo registar al ciente",
+                    content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = APIResponse.class)),
+
+                    }),
+    })
+    public ResponseEntity<APIResponse> save(@RequestBody Client client) {
+        APIResponse response = clientService.save(client);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
 
 }
--- src/main/java/utez/edu/mx/unidad3/models/client/ClientService.java
+++ src/main/java/utez/edu/mx/unidad3/models/client/ClientService.java
@@ -23,13 +23,37 @@ public class ClientService {
     }
 
     @Transactional(readOnly = true)
-    public APIResponse findById() {
-        return null;
+    public APIResponse findById(Long id) {
+        try {
+            Client found = clientRepository.findById(id).orElse(null);
+            if (found == null){
+                return new APIResponse("No se encontro al cliente solicitado",true, HttpStatus.NOT_FOUND);
+            }
+
+            return new APIResponse("Operacion exitosa",found,false, HttpStatus.OK);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new APIResponse("Error al buscar el cliente solicitado",false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
-    @Transactional(readOnly = true)
-    public APIResponse save() {
-        return null;
+    @Transactional(rollbackFor = Exception.class)
+    public APIResponse save(Client client) {
+
+        try {
+            Client saved = clientRepository.save(client);
+
+            if (saved == null){
+                return new APIResponse("No se guardo el cliente",true, HttpStatus.NOT_FOUND);
+            }
+
+            return new APIResponse("Operacion exitosa",false, HttpStatus.CREATED);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return new APIResponse("Error al guardar el cliente",false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
     @Transactional(readOnly = true)
     public APIResponse update() {
